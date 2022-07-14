5086681dfa8f3f4185a1131408a41c8591c2a5df
Hubert <kingzly37@gmail.com>
2022-07-14 12:38:47 +0200

    1.2

--- coderoad/test/tutorial.test.js
+++ coderoad/test/tutorial.test.js
@@ -25,3 +25,46 @@ describe('javascript-tutorial-2 folder', () => {
     assert(rootDir.indexOf('main.js') >= 0)
   })
 })
+
+// 1.2
+
+const readFile = util.promisify(fs.readFile)
+const getJsFile = async (dir = process.cwd()) => {
+  const pathToJs = path.join(dir, '..', 'main.js')
+  const jsFile = await readFile(pathToJs)
+
+  if (!jsFile) {
+    throw new Error(`Could not find ${pathToJs}`)
+  }
+  return jsFile
+}
+
+const { products, fruits, vegetables, cheaperProducts } = require('../../main.js')
+
+describe('main.js', () => {
+  let jsFile
+  before(async () => {
+    jsFile = await getJsFile()
+  })
+
+  it('main.js should contain \"products\" array', () => {
+    assert.ok(products, "The \"products\" array does not exist");
+    assert.ok(Array.isArray(products), "\"products\" is not an array");
+  })
+
+  it('main.js should contain \"fruits\" array', () => {
+    assert.ok(fruits, "The \"fruits\" array does not exist");
+    assert.ok(Array.isArray(fruits), "\"fruits\" is not an array");
+  })
+
+  it('main.js should contain \"vegetables\" array', () => {
+    assert.ok(vegetables, "The \"vegetables\" array does not exist");
+    assert.ok(Array.isArray(vegetables), "\"vegetables\" is not an array");
+  })
+
+  it('main.js should contain \"cheaperProducts\" array', () => {
+    assert.ok(cheaperProducts, "The \"cheaperProducts\" array does not exist");
+    assert.ok(Array.isArray(cheaperProducts), "\"cheaperProducts\" is not an array");
+  })
+
+})
