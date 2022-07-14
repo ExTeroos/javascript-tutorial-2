6c893b00b51d5c48b532d94abca23f1ab8217355
Hubert <kingzly37@gmail.com>
2022-07-14 12:38:51 +0200

    1.2S

---
+++ main.js
@@ -0,0 +1,50 @@
+let products = [
+    {
+        "name": "Tomato",
+        "type": "Fruit",
+        "price": 5
+    },
+
+    {
+        "name": "Potato",
+        "type": "Vegetable",
+        "price": 2
+    },
+
+    {
+        "name": "Banana",
+        "type": "Fruit",
+        "price": 3
+    },
+
+    {
+        "name": "Kiwi",
+        "type": "Fruit",
+        "price": 4
+    },
+
+    {
+        "name": "Carrot",
+        "type": "Vegetable",
+        "price": 6
+    },
+
+    {
+        "name": "Onion",
+        "type": "Vegetable",
+        "price": 3
+    }
+]
+
+let fruits = [];
+let vegetables = [];
+let cheaperProducts = [];
+
+// DO NOT CHANGE THE CODE ABOVE
+// Your space to code is below this line
+
+
+
+// Your space to code is above this line
+// DO NOT CHANGE THE CODE BELOW
+module.exports = { products, fruits, vegetables, cheaperProducts };
